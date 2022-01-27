e2fe02d846630c6f5826e8da648cdce8308d4eab
Rafa Al Razzak <94893946+raf-ar@users.noreply.github.com>
2022-01-27 09:34:48 +0000

    .

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -9,14 +9,13 @@ import ThemeSwitch from './ThemeSwitch'
 const LayoutWrapper = ({ children }) => {
   return (
     <SectionContainer>
-      <div className="flex flex-col justify-between h-screen">
-        <header className="flex items-center justify-between py-10">
+      <div className="flex flex-col  justify-between h-screen">
+        <header className=" flex items-center justify-between py-10">
           <div>
             <Link href="/" aria-label="</raf> Blog">
               <div className="flex items-center justify-between">
-                <div className="mr-3 bg-gradient-to-br from-emerald-300 to-cyan-300 text-2xl font-bold bg-clip-text text-transparent">{`<raf />`}</div>
                 {typeof siteMetadata.headerTitle === 'string' ? (
-                  <div className="hidden h-6 text-2xl font-semibold sm:block">
+                  <div className="h-6 text-2xl font-semibold mr-3 bg-gradient-to-br from-emerald-300 to-cyan-300 text-2xl font-bold bg-clip-text text-transparent">
                     {siteMetadata.headerTitle}
                   </div>
                 ) : (
