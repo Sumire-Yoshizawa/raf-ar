b5dce5c0a8b5eba7011fcba8bb53fdb90a42ac21
Rafa Al Razzak <94893946+raf-ar@users.noreply.github.com>
2022-01-28 00:12:10 +0000

    .

--- components/LayoutWrapper.js
+++ components/LayoutWrapper.js
@@ -1,6 +1,5 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
 import Link from './Link'
 import SectionContainer from './SectionContainer'
 import Footer from './Footer'
@@ -10,8 +9,8 @@ import ThemeSwitch from './ThemeSwitch'
 const LayoutWrapper = ({ children }) => {
   return (
     <SectionContainer>
-      <div className="flex flex-col justify-between h-screen">
-        <header className="flex items-center justify-between py-10">
+      <div className="flex flex-col  justify-between h-screen">
+        <header className=" flex items-center justify-between py-10">
           <div>
             <Link href="/" aria-label="</raf> Blog">
               <div className="flex items-center justify-between">
